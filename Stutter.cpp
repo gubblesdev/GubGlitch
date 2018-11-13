c9f7db94e09d5dd6a04287c498ce22df6a989df8
Gubbles <jakehawesaudio@gmail.com>
2018-11-13 18:36:23 -0500

    Replace Arrays with Vectors
    -cause variable length arrays are a bitchh

--- Stutter.cpp
+++ Stutter.cpp
@@ -2,36 +2,28 @@
 #include "IPlug/IPlug_include_in_plug_hdr.h"
 
 
-Stutter::Stutter() {
-
-}
-
 void Stutter::setLength(double tempo, double division, double sampleRate) {
 	// Calculate how many samples long the deck should be
 	// Integer division truncates remainder; determine whether or not this is desirable behavior
 	deckLength = ((tempo / 60) / division) * sampleRate;		// Subtract 1 because first index of array is 0?
 	c = 0;
 	isFull = false;
+	isActive = true;
 }
 
 double Stutter::process(double input) {
-	
-	if (c > deckLength) {
-		c = 0;
-	}
 
 	// Fill the stutter deck until it's full
 	if (!isFull) {
-		deck[c] = input;
-		c++;
-		if (c > deckLength)
+		deck[c++] = input;
+		if (c == deckLength)
 			isFull = true;
 		return input;
 	}
 	// Loop the deck after it's been filled
 	else {
-		stage = deck[c];
-		c++;
-		return stage;
+		if (c == deckLength)
+			c = 0;
+		return deck[c++];
 	}
 }
--- Stutter.h
+++ Stutter.h
@@ -2,20 +2,27 @@
 #ifndef __STUTTER__
 #define __STUTTER__
 #include <math.h>
+#include <vector>
 #include "IPlugStructs.h"
 
 class Stutter {
 private:
-	double* deck;
+	bool isActive;
+	std::vector<double> deck;
 	int deckLength;
 	int c;
 	bool isFull;
-	double stage;
 
 public:
 	void setLength(double tempo, double division, double sampleRate);
 	double process(double input);
-	Stutter();
+	inline void setActive(bool muted) { isActive = muted; }
+	Stutter() :
+		isActive(false),
+		c(0),
+		isFull(false) {
+		;
+	}
 };
 
 #endif
